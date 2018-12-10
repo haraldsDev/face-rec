0ff90c35b7150a8892f7c0640077961e4367b5ae
haraldsDev <40548378+haraldsDev@users.noreply.github.com>
2018-12-10 03:04:21 +0200

    Create Logo.js

---
+++ src/Components/Logo/Logo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import './Logo.css';
+import lightning from './lightning.png';
+
+const Logo = () => {
+	return (
+		<div className='ma4 mt0'>
+
+			<Tilt className="Tilt br2 shadow-2" options={{ max : 35 }} style={{ height: 180, width: 150 }} >
+			 	<div className="Tilt-inner pt3"> 
+			 		<img src={lightning} alt='logo'/> 
+			 	</div>
+			</Tilt>
+		</div>
+	);
+}
+
+export default Logo;
