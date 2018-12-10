0d777d0aca8c8feda684d2b2000336501187bbe5
haraldsDev <40548378+haraldsDev@users.noreply.github.com>
2018-12-10 03:12:09 +0200

    Create FaceRecognition.js

---
+++ src/Components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FaceRecognition = () => {
+	return (
+		<div className='center'>
+			<img 
+				src={'https://samples.clarifai.com/face-det.jpg'} 
+				alt='Face-will-be-here'
+			/>
+		</div>
+	);
+}
+
+export default FaceRecognition;
