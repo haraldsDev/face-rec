d42a0d6d64544ff63b7f20714a0ee6bd827c07bf
haraldsDev <Haralds.Dev@gmail.com>
2018-12-14 00:34:05 +0200

    displayFaceBox() created, FaceRecognition css file created & info from clarifai copied to it, .bounding-box class with style created in FaceRecognition.js

--- src/Components/FaceRecognition/FaceRecognition.js
+++ src/Components/FaceRecognition/FaceRecognition.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, box }) => {
 	return (
 		<div className='center ma'>
 			<div className='absolute mt2'>
@@ -8,9 +9,11 @@ const FaceRecognition = ({ imageUrl }) => {
 					src={imageUrl}
 					id='inputimage'
 					alt='Face-will-be-here' 
-					width='500px'
+					width='300px'
 					height='auto'
 				/>
+			</div>
+			<div className="bounding-box" style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}>
 			</div>
 		</div>
 	);
