6ab1bad7dddb79b44ae8cdd1a2beb7d301560ab3
haraldsDev <Haralds.Dev@gmail.com>
2018-12-30 13:04:29 +0200

    routing and layout review

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from 'react'; 
 
 const Navigation = ({ onRouteChange, isSignedIn }) => {
 		if(isSignedIn) {
 			return(
 			<nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
-				<p onClick={() => onRouteChange('Signout')} className="f3 link dim black underline pa3 pointer">Sign out</p>
+				<p onClick={() => onRouteChange('SignIn')} className="f3 link dim black underline pa3 pointer">Sign out</p>
 			</nav>	
 			)
 		} else {
